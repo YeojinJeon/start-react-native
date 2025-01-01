f90d157bf35592fc75b6ef1aa2234e9d33ddc068
yeojin_jeon <jeonyeojin1993@gmail.com>
2025-01-01 14:48:02 +0900

    MyButton 스타일링

--- src/components/MyButton.js
+++ src/components/MyButton.js
@@ -3,8 +3,15 @@ import { Text, TouchableOpacity } from "react-native";
 
 const MyButton = () => {
     return (
-        <TouchableOpacity>
-            <Text style={{ fontSize: 24 }}>MyButton</Text>
+        <TouchableOpacity
+            style={{
+                backgroundColor: '#3498db',
+                padding: 16,
+                margin: 10,
+                borderRadius: 8,
+            }}
+            onPress={() => alert('Press MyButton!!!')}>
+            <Text style={{ color: 'white', fontSize: 24 }}>MyButton</Text>
         </TouchableOpacity>
     );
 };
